355d68be92f83a584c314355dc667546b72757d4
Martin <martin@marvhus.xyz>
2025-07-07 18:06:25 +0200

    Made builder walk all files in sites/
    Next is copying over files from web/ and doing collision detection.
    Maybe this could just be done manually?  Or should this be a part of the builder?

--- tools/builder.cpp
+++ tools/builder.cpp
@@ -1,78 +1,17 @@
+#include <fstream>
 #include <string_view>
 #include <iostream>
-#include <assert.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <sstream>
+#include <filesystem>
 
 #include <md4c/md4c.h>
 #include <md4c/md4c-html.h>
 
-std::string_view read_entire_file(const char* path) {
-    FILE* fp = fopen(path, "r");
-    if (fp == nullptr) {
-        std::cout << "Unable to open file: " << path << std::endl;
-        exit(1);
-    }
-
-    if (int status = fseek(fp, 0, SEEK_END); status != 0) {
-        std::cout << "Unable to find end of file for: " << path << std::endl;
-        exit(1);
-    }
-
-    long length = ftell(fp);
-
-    rewind(fp);
-
-    char* data = (char*)malloc(length);
-    if (size_t bytes_read = fread(data, 1, length, fp); bytes_read != (size_t)length) {
-        std::cout << "Unable to read '" << path << "'.  Read " << bytes_read << " but expected " << length << std::endl;
-        exit(1);
-    }
-
-    if (int result = fclose(fp); result != 0) {
-        std::cout << "Unable to close file: " << path << std::endl;
-        exit(1);
-    }
-
-    std::string_view sv(data, length);
-    return sv;
-}
-
-void write_entire_file(const char* path, std::string_view content) {
-    FILE* fp = fopen(path, "w");
-    if (fp == nullptr) {
-        std::cout << "Unable to open file: " << path << std::endl;
-        exit(1);
-    }
-
-    size_t content_length = content.length();
-    if (size_t bytes_written = fwrite(content.data(), 1, content_length, fp); bytes_written != content_length) {
-        std::cout << "Unable to write to file: " << path << std::endl;
-        exit(1);
-    }
-
-    if (int result = fclose(fp); result != 0) {
-        std::cout << "Unable to close file: " << path << std::endl;
-        exit(1);
-    }
-}
-
-std::string_view strip_pages_path(std::string_view path) {
-    constexpr std::string_view prefix_path("pages/");
-    constexpr size_t prefix_length = prefix_path.length();
-
-    size_t path_length = path.length();
-    {
-        bool has_prefix = path.starts_with(prefix_path);
-        bool path_after_prefix = path_length > prefix_length;
-        if (!has_prefix || !path_after_prefix) {
-            std::cout << "'" << path << "' is not a valid pages path" << std::endl;
-            exit(1);
-        }
-    }
-
-    return path.substr(prefix_length, path_length - prefix_length);
+std::string read_entire_file(std::string path) {
+    std::ifstream file(path);
+    std::stringstream content;
+    content << file.rdbuf();
+    return content.str();
 }
 
 void handle_chunk(const MD_CHAR* data, MD_SIZE length, void* userdata) {
@@ -81,31 +20,77 @@ void handle_chunk(const MD_CHAR* data, MD_SIZE length, void* userdata) {
     *html_builder << view;
 }
 
-void build_html(const char* path) {
-    std::string_view content = read_entire_file(path);
+void build_html(std::string path, std::string output) {
 
     std::stringstream html_builder;
     html_builder << "<!DOCTYPE html>\n<html>\n"
-                 << read_entire_file("page-head.html")
+                 << read_entire_file("templates/head.html") // @TODO: read from config file
                  << "<body>\n"
-                 << read_entire_file("page-navbar.html")
+                 << read_entire_file("templates/navbar.html") // @TODO: read from config file
                  << "<main>\n";
 
-    if (int result = md_html(content.data(), content.length(), handle_chunk, &html_builder,
-                             MD_FLAG_COLLAPSEWHITESPACE | MD_FLAG_STRIKETHROUGH,
-                             MD_HTML_FLAG_DEBUG); result != 0) {
-        std::cout << "Failed to parse and render markdown." << std::endl;
-        exit(1);
+    { // Read, Parse, and Render MD file to HTML
+        std::string content = read_entire_file(path);
+        if (int result = md_html(content.data(), content.length(), handle_chunk, &html_builder,
+                                 MD_FLAG_COLLAPSEWHITESPACE | MD_FLAG_STRIKETHROUGH,
+                                 MD_HTML_FLAG_DEBUG); result != 0) {
+            std::cout << "Failed to parse and render markdown." << std::endl;
+            exit(1);
+        }
     }
 
     html_builder << "</main>\n"
-                 << read_entire_file("page-footer.html")
+                 << read_entire_file("templates/footer.html") // @TODO: read from config file
                  << "</body>\n</html>";
 
-    write_entire_file("web/index.html", html_builder.str());
-    if (void* data = (void*)content.data(); data) free(data);
+    { // Create parrent directory
+        std::filesystem::path file_path(output);
+        std::filesystem::create_directory(file_path.parent_path());
+    }
+
+    { // Write entire file
+        std::ofstream file(output);
+        file << html_builder.str();
+        file.close();
+    }
+
+    std::cout << path << " -> " << output << std::endl;
+}
+
+void walk_directory(std::string_view directory_path) {
+    for (const std::filesystem::directory_entry& entry: std::filesystem::directory_iterator(directory_path)) {
+        if (entry.is_directory()) {
+            walk_directory(entry.path().string());
+            continue;
+        }
+        // @TODO: check if file is .md file?
+
+        std::stringstream result_path;
+        result_path << "public/";
+
+        std::string tmp_path = entry.path().string();
+        int start = 0, end = tmp_path.length();
+
+        constexpr std::string_view PREFIX = "pages/", SUFFIX = ".md"; // @TODO: read from config file
+        if (tmp_path.starts_with(PREFIX)) {
+            int length = PREFIX.length();
+            start += length;
+            end -= length;
+        }
+        if (tmp_path.ends_with(SUFFIX)) {
+            int length = SUFFIX.length();
+            end -= length;
+        }
+        std::string tmp_path2 = tmp_path.substr(start, end);
+
+        result_path << tmp_path2;
+
+        result_path << ".html";
+
+        build_html(entry.path().string(), result_path.str());
+    }
 }
 
 int main(void) {
-    build_html("pages/index.md");
+    walk_directory("pages/"); // @TODO: read from config file
 }
